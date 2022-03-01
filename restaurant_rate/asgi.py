034d745dac21eed95c88b07704fc343ab1d42122
bharu1393 <rajatbhardwaj@Rajats-MacBook-Pro.local>
2022-03-01 13:39:15 +0000

    initial commit

---
+++ restaurant_rate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for restaurant_rate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_rate.settings')
+
+application = get_asgi_application()
