aa7e5f8852156ce7d86cd4e86ee0ccf49bc9eb4e
XiaoyangWang <2626395W@student.gla.ac.uk>
2022-03-03 10:11:43 +0000

    create css js static and media folders

--- restaurant_rate/settings.py
+++ restaurant_rate/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
-#MEDIA_DIR = os.path.join(BASE_DIR,'media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL  = '/media/'
+
+PASSWORD_HASHERS = ( 'django.contrib.auth.hashers.PBKDF2PasswordHasher', 
+                    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
