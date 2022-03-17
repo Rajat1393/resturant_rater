00a6809bae83602f0c80d544a543699352e8342f
XiaoyangWang <2626395W@student.gla.ac.uk>
2022-03-17 18:27:49 +0000

    fixed index error

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,38 @@
 $(document).ready(function() {
 
     $("#brunchClub").click(function() {
+        var a = "The Brunch Club";
+        $("#submitValue").val(a);
         $("#targetForm").submit();
     });
 
     $("#Tantrum").click(function() {
+        var tantrum = "Tantrum Doughnuts";
+        $("#submitValue").val(tantrum);
         $("#targetForm").submit();
     });
 
     $("#Five").click(function() {
+        var five = "Five March"
+        $("#submitValue").val(five);
         $("#targetForm").submit();
     });
 
     $("#Mister").click(function() {
+        var mister = "Mister Singh's India";
+        $("#submitValue").val(mister);
         $("#targetForm").submit();
     });
 
     $("#Buck").click(function() {
+        var buck = "Buck's BarRestaurant";
+        $("#submitValue").val(buck);
         $("#targetForm").submit();
     });
 
     $("#Garden").click(function() {
+        var garden = "The Quay China Garden";
+        $("#submitValue").val(garden);
         $("#targetForm").submit();
     });
 })
